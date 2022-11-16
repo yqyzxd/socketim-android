47d4cc352d60edd29b5ca12af121208d207cc1d0
windq <shihaowind@163.com>
2022-11-16 13:45:48 +0800

    ConcurrentModificationException

--- socket_im/src/main/java/com/wind/im/core/Observers.java
+++ socket_im/src/main/java/com/wind/im/core/Observers.java
@@ -7,29 +7,30 @@ import java.util.List;
 
 public class Observers {
 
-    private List<Observer<List<IMessage>>> mMessageObservers=new ArrayList<>();
-    private List<Observer<Void>> mOpenObservers=new ArrayList<>();
-    private List<Observer<ServerCommand>> mServerBroadcastObservers=new ArrayList<>();
+    private List<Observer<List<IMessage>>> mMessageObservers = new ArrayList<>();
+    private List<Observer<Void>> mOpenObservers = new ArrayList<>();
+    private List<Observer<ServerCommand>> mServerBroadcastObservers = new ArrayList<>();
     private static Observers sInstance;
-    public static  Observers getInstance(){
-        if (sInstance==null){
-            synchronized (Observers.class){
-                if (sInstance==null){
-                    sInstance=new Observers();
+
+    public static Observers getInstance() {
+        if (sInstance == null) {
+            synchronized (Observers.class) {
+                if (sInstance == null) {
+                    sInstance = new Observers();
                 }
             }
         }
         return sInstance;
     }
 
-    public  void observeReceiveMessages(Observer<List<IMessage>> observer,boolean register){
+    public void observeReceiveMessages(Observer<List<IMessage>> observer, boolean register) {
         if (register)
             mMessageObservers.add(observer);
         else
             mMessageObservers.remove(observer);
     }
 
-    public void observeSocketOpen(Observer<Void> observer,boolean register){
+    public void observeSocketOpen(Observer<Void> observer, boolean register) {
         if (register)
             mOpenObservers.add(observer);
         else
@@ -37,7 +38,7 @@ public class Observers {
     }
 
     public void observeServerBroadcast(Observer<ServerCommand> observer
-                                       ,boolean register){
+            , boolean register) {
         if (register)
             mServerBroadcastObservers.add(observer);
         else
@@ -46,29 +47,45 @@ public class Observers {
 
     /**
      * 派发聊天消息
+     *
      * @param messages
      */
-    public void dispatchMessages(List<IMessage> messages){
-        for (Observer<List<IMessage>> observer:mMessageObservers){
-            observer.onEvent(messages);
+    public void dispatchMessages(List<IMessage> messages) {
+        try {
+            for (Observer<List<IMessage>> observer : mMessageObservers) {
+                observer.onEvent(messages);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     /**
      * 派发socket连接成功消息
      */
     public void dispatchOpened() {
-        for (Observer<Void> observer:mOpenObservers){
-            observer.onEvent(null);
+        try {
+            for (Observer<Void> observer : mOpenObservers) {
+                observer.onEvent(null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     /**
      * 派发服务端的透传
      */
-    public void dispatchServerBroadcast(ServerCommand serverCommand){
-        for (Observer<ServerCommand> observer:mServerBroadcastObservers){
-            observer.onEvent(serverCommand);
+    public void dispatchServerBroadcast(ServerCommand serverCommand) {
+        try {
+            for (Observer<ServerCommand> observer : mServerBroadcastObservers) {
+                observer.onEvent(serverCommand);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
